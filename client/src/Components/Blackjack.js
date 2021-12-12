73e4fbd00fd2ccc96768ee35f2103908bdd5d119
Dario Mercuri <dario.mercuri31@gmail.com>
2021-12-11 21:04:06 -0500

    set up initial blackjack deal

--- client/src/Components/Blackjack.js
+++ client/src/Components/Blackjack.js
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
 
 //player makes bet
 //round starts
 //cards are dealt
+//disable betting
 //hit or stand
 //did player win or lose?
 //reset
@@ -10,26 +12,73 @@ import React, { useState } from "react";
 export const Blackjack = () => {
     const [balance, setbalance] = useState(1000);
     const [bet, setBet] = useState(100);
+    const [playerHand, setPlayerHand] = useState([]);
+    const [dealerHand, setDealerHand] = useState([]);
+    const [deck, setDeck] = useState();
+
+    //Create Deck
+    useEffect(() => {
+        fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6`)
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                setDeck(data);
+            });
+    }, []);
+
+    //Deal Cards
+    function dealCards() {
+        console.log("DEAL");
+        fetch(
+            `https://deckofcardsapi.com/api/deck/${deck.deck_id}/draw/?count=4`
+        )
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                setPlayerHand(data.cards.slice(0, 2));
+                setDealerHand(data.cards.slice(2, 4));
+            });
+    }
+
     return (
-        <div>
-            <p>Your Balance: {balance}</p>
-            <h1>BlackJack</h1>
-            <hr />
-            <h2>Dealers Hand</h2>
-            <hr />
-            <h2>Your Hand</h2>
-            <hr />
-            <p>Your Balance: </p>
-            <input
-                value={bet}
-                onChange={(e) => {
-                    setBet(e.target.value);
-                }}
-            />
-            <button disabled={!bet}>Bet</button>
-            <hr />
-            <button>Hit</button>
-            <button>Stand</button>
-        </div>
+        <>
+            {!deck ? (
+                <p>loading</p>
+            ) : (
+                <div>
+                    <h1>BlackJack</h1>
+                    <hr />
+                    <h2>Dealers Hand</h2>
+                    {dealerHand.map((card, k) => {
+                        return <Card src={card.image} />;
+                    })}
+                    <hr />
+                    <h2>Your Hand</h2>
+                    {playerHand.map((card, k) => {
+                        return <Card src={card.image} />;
+                    })}
+                    <hr />
+                    <p>Your Balance: {balance}</p>
+                    <input
+                        value={bet}
+                        onChange={(e) => {
+                            setBet(e.target.value);
+                        }}
+                    />
+                    <button disabled={!bet} onClick={() => dealCards()}>
+                        Bet
+                    </button>
+                    <hr />
+                    <button>Hit</button>
+                    <button>Stand</button>
+                </div>
+            )}
+        </>
     );
 };
+
+const Card = styled.img`
+    width: 10%;
+    height: auto;
+    margin: 5px;
+`;
