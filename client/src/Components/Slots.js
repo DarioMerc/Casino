e2730da7cb59d9db9eb9ecb882c3bf83309829ec
Dario Mercuri <dario.mercuri31@gmail.com>
2022-01-13 23:40:01 -0500

    started slots logic

--- client/src/Components/Slots.js
+++ client/src/Components/Slots.js
@@ -1,19 +1,46 @@
 import React from 'react'
 import styled from 'styled-components'
 
+const fruits = ["🍒", "🍉", "🍊", "🍓", "🍇", "🥝"]
+
 const Slots = () => {
+    //MAKE THIS AN OBJECT WITH 3 SLOTS
+    const [slots,setSlots] = setState()
+    function roll(){
+        console.log("ROLLING")
+        const slot1 = fruits[
+            Math.floor(Math.random() * fruits.length)
+        ];
+        const slot2 = fruits[    
+            Math.floor(Math.random() * fruits.length)
+        ];
+
+        const slot3 = fruits[    
+            Math.floor(Math.random() * fruits.length)
+        ];
+        //SET SLOTS
+        console.log(slot1,slot2,slot3)
+        // setState({slot1: slot1, slot2:slot2, slot3:slot3, rolling: true});
+        setTimeout(() => {
+            //SHOW SLOTS TO PLAYER
+            // this.setState({ rolling: false });
+        }, 700)
+    }
+
     return (
         <Container>
             <h1>Slots</h1>
+            <p>-</p>
             <SlotContainer>
                 <Slot>1</Slot>
                 <Slot>2</Slot>
                 <Slot>3</Slot>
             </SlotContainer>
             <BetContainer>
-            <p>Bet:</p>
-            <input value={10}></input>
-            <button>Spin</button>
+                <span>
+                    <p>Bet:</p><input value={10}></input>
+                </span>
+                <button onClick={() => roll()}>Spin</button>
             </BetContainer>
         </Container>
     )
@@ -36,6 +63,6 @@ const Slot = styled.div`
 `
 const BetContainer = styled.div`
     display: flex;
-    flex-direction: row;
+    flex-direction: column;
 `
 export default Slots
